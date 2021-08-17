68b669fff3d4551e77431c5da9e0fd1c065e34fa
Rodrigo de Oliveira <rodrigo.oliveira@amigoedu.com.br>
2021-08-17 00:46:29 -0300

    feat: move size filter from Products to Detail page and disable 'Add to cart' button if no size has been chosen

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
@@ -8,6 +8,7 @@ export default function Detail() {
   const { id } = useParams();
   const navigate = useNavigate();
   const { data: product, loading, error } = useFetch(`products/${id}`);
+  const [sku, setSku] = useState('')
 
   if (loading) return <Spinner />;
   if (!product) return <PageNotFound />;
@@ -18,8 +19,18 @@ export default function Detail() {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+
+        <select
+          id="size"
+          value={sku}
+          onChange={(e) => setSku(e.target.value)}
+        >
+          <option value="">What size?</option>
+          {product?.skus?.map((sku) => <option key={sku.sku} value={sku.sku}>{sku.size}</option>)}
+        </select>
+        
       <p>
-        <button className="btn btn-primary" onClick={() => navigate("/cart")}>
+        <button className="btn btn-primary" onClick={() => navigate("/cart")} disabled={!sku}>
           Add to cart
         </button>
       </p>
--- src/Products.jsx
+++ src/Products.jsx
@@ -36,17 +36,6 @@ export default function Products() {
   return (
     <>
       <section id="filters">
-        <label htmlFor="size">Filter by Size:</label>{" "}
-        <select
-          id="size"
-          value={size}
-          onChange={(e) => setSize(e.target.value)}
-        >
-          <option value="">All sizes</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-        </select>
         {size && <h2>Found {filteredProducts.length} items</h2>}
       </section>
       <section id="products">{filteredProducts.map(renderProduct)}</section>
